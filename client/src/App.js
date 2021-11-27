013f27b1ed6c059636aa5a276196be07fd1f0691
drip016 <73792907+drip016@users.noreply.github.com>
2021-11-27 02:01:08 +0100

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Box from './components/box.jsx';
+import React from 'react'
 
-function App() {
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>TEST HEADING</h1>
-        {/* <Box heading="Box a heading 123" contentText="Hallo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt allo tedasxt Hallo text Hallo text Hallo text Hallo text Hallo text Hallo text Hallo text Hallo text "/> */}
-
-      </header>
+    <div>
+        <h1>Pigeoner</h1>
     </div>
   );
 }
