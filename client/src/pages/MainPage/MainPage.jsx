fc53a7d3428e3e87bffee5b0aa56a0329efa50ae
drip016 <73792907+drip016@users.noreply.github.com>
2021-12-03 23:25:06 +0100

    folder changing

---
+++ client/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import SinglePost from '../../components/SinglePost/SinglePost'
+import './MainPage.css'
+
+const MainPage = () => {
+    return (
+        <div className="mainpage">
+            <div className="sidebar">
+                <h1>SIDEBAR</h1>
+            </div>
+            <div className="postsSection">
+                <SinglePost />
+                <SinglePost />
+                <SinglePost />
+                <SinglePost />
+                <SinglePost />
+                <SinglePost />
+                <SinglePost />
+                <SinglePost />
+                <SinglePost />
+            </div>
+            <div className="rightSidebar">
+                <h1>SIDEBAR</h1>
+            </div>
+        </div>
+    )
+}
+
+export default MainPage
